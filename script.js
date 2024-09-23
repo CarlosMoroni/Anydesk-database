e14c3fa678eb1ba64b7e7106c58a49bf035ff40d
Carlos Moroni <carlosmoroni123@gmail.com>
2024-09-23 08:47:52 -0300

    pequenas mudanças no front end e instalação do express para criação do back end

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//fazer tudo
+function openEditNew() {
+    let addNew = document.querySelector('#addNewDevice');
+    let dialogNewEdit = document.querySelector('#add-to-edit')
+    let closeDialog = document.querySelector('#closeDialog')
+
+    addNew.addEventListener("click", () => {
+        dialogNewEdit.classList.toggle("ativa")
+    })
+
+    closeDialog.addEventListener("click", () => {
+        dialogNewEdit.classList.toggle("ativa")
+    })
+}
+
+
+
+openEditNew();
