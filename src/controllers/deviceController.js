594bacb4bb5c304410fbbc0be0e05cd0d9122be9
Carlos Moroni <carlosmoroni123@gmail.com>
2024-10-08 15:25:00 -0300

    end-point de buscar por termo de busca feito

--- src/controllers/deviceController.js
+++ src/controllers/deviceController.js
@@ -1,3 +1,4 @@
+const { Op } = require('sequelize');
 const Device = require('../models/Device');
 
 module.exports = {
@@ -30,21 +31,50 @@ module.exports = {
         }
     },
 
-    async getByIdDevice(req, res){
+    async getByIdDevice(req, res) {
         try {
             const id = req.params.id;
-            
-            if(!id){
-                res.json({message: "objeto nao encontado"})
+
+            if (!id) {
+                res.json({ message: "objeto nao encontado" })
             }
             const device = await Device.findByPk(id);
-            if(!device) {
-                res.json({message: "objeto nao encontado!"})
+            if (!device) {
+                res.json({ message: "objeto nao encontado!" })
             }
 
-            return res.status(200).json(device); 
+            return res.status(200).json(device);
         } catch (error) {
             console.error("erro em device controller, getByIdDevice() " + error);
+            res.status(500);
+        }
+    },
+
+    async getBySearchTerm(req, res) {
+        const { search_term } = req.query;
+
+        if (!search_term) {
+            return res.status(400).json({ message: 'Termo de busca não fornecido' });
+        };
+
+        try {
+            const devices = await Device.findAll({
+                where: {
+                    [Op.or]: [
+                        { name_device: { [Op.like]: `%${search_term}` } },
+                        { access_code: { [Op.like]: `%${search_term}` } },
+                        { department: { [Op.like]: `%${search_term}` } },
+                    ]
+                }
+            })
+
+            if(!devices.length){
+                return res.status(200).json({message: 'nenhum dispositivo encontrado'});
+            }
+
+            return res.status(200).json(devices);
+        } catch (error) {
+            return console.error('Erro no getBySearchTerm(): ' + error);
             res.status(500);
         }
     },
@@ -53,20 +83,20 @@ module.exports = {
         try {
             const id = req.params.id;
 
-            if(!id) {
-                res.json({message: "id nao especificado!"});
+            if (!id) {
+                res.json({ message: "id nao especificado!" });
             };
             const deviceResearch = await Device.findByPk(id);
             if (!deviceResearch) {
-                res.json({message: "objeto nao encontado!"});
+                res.json({ message: "objeto nao encontado!" });
             };
 
             const device = req.body;
             await Device.update(device, {
-                where: {id: id},
+                where: { id: id },
             });
 
-            return res.status(201).json({message: "Objeto atualizado com sucesso!"});
+            return res.status(201).json({ message: "Objeto atualizado com sucesso!" });
         } catch (error) {
             console.error("erro em device controller, updateDevice() " + error);
             res.status(500);
@@ -78,16 +108,16 @@ module.exports = {
             const id = req.params.id;
 
             if (!id) {
-                res.json({message: "id não especificado!"});
+                res.json({ message: "id não especificado!" });
             }
             const device = await Device.findByPk(id);
             if (!device) {
-                res.json({message: "objeto não encontrado!"})
-            }else {
+                res.json({ message: "objeto não encontrado!" })
+            } else {
                 await device.destroy();
             }
 
-            return res.status(201).json({ message: "Objeto excluido com sucesso!"});
+            return res.status(201).json({ message: "Objeto excluido com sucesso!" });
         } catch (error) {
             console.error("erro em device controller, deleteDevice() " + error);
             res.status(500);
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,6 +5,7 @@ const routes = express.Router();
 
 routes.post('/device', DeviceController.createDevice);
 routes.get('/device', DeviceController.getAllDevice);
+routes.get('/device/buscar?', DeviceController.getBySearchTerm);
 routes.get('/device/:id', DeviceController.getByIdDevice);
 routes.put('/device/:id', DeviceController.updateDevice);
 routes.delete('/device/:id', DeviceController.deleteDevice);
